4e4c937e9ca9cbc5d783feeb8a3a144ae5a2a729
jay-bebe <jaybebe7676@gmail.com>
2023-09-20 12:14:13 +1000

    Initial commit

---
+++ src/main/java/net/pokepalms/palmsmod/PalmsModClient.java
@@ -0,0 +1,10 @@
+package net.pokepalms.palmsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PalmsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/pokepalms/palmsmod/PalmsModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.pokepalms.palmsmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PalmsModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
