06f4cd19d09c149f1baf1931d7eece2a64b8c249
Zanckor <zanckor002@gmail.com>
2024-06-18 19:15:15 +0200

    Fixed animation bugs

--- src/main/java/dev/zanckor/cobblemonrider/mixin/PokemonMixin.java
+++ src/main/java/dev/zanckor/cobblemonrider/mixin/PokemonMixin.java
@@ -89,7 +89,7 @@ public abstract class PokemonMixin extends PathfinderMob implements Poseable, Sc
 
     @Inject(method = "tick", at = @At("TAIL"))
     public void tick(CallbackInfo ci) {
-        if (getControllingPassenger() != null) {
+        if (getControllingPassenger() != null && getControllingPassenger() instanceof Player player) {
             if (cobblemonRiding$mayMountOtherEntities() && canAddPassenger(getControllingPassenger())) {
                 cobblemonRiding$mountEntity();
             }
