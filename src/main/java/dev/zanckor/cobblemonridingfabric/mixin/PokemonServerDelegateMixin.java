cd0a3486e4152417ab7644f78c2b725064e34da8
Zanckor <zanckor002@gmail.com>
2024-06-18 20:28:07 +0200

    Fixed animation bugs on battle, and swimming

--- src/main/java/dev/zanckor/cobblemonridingfabric/mixin/PokemonServerDelegateMixin.java
+++ src/main/java/dev/zanckor/cobblemonridingfabric/mixin/PokemonServerDelegateMixin.java
@@ -3,7 +3,6 @@ package dev.zanckor.cobblemonridingfabric.mixin;
 
 import com.cobblemon.mod.common.entity.pokemon.PokemonEntity;
 import com.cobblemon.mod.common.entity.pokemon.PokemonServerDelegate;
-import net.minecraft.util.math.Vec3d;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,7 +12,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PokemonServerDelegate.class)
 public abstract class PokemonServerDelegateMixin {
-    private Vec3d prevPos;
 
     @Shadow
     public abstract PokemonEntity getEntity();
@@ -21,10 +19,16 @@ public abstract class PokemonServerDelegateMixin {
 
     @Inject(method = "updatePoseType", at = @At("HEAD"), remap = false)
     private void head(CallbackInfo ci) {
-        if(this.getEntity().getControllingPassenger() != null){
-            boolean isMoving = getEntity().getControllingPassenger().getVelocity().lengthSquared() > 0.0062D;
+        float minimumSpeed = getEntity().isTouchingWater() ? 0.00026F : 0.0062F;
 
-            getEntity().getDataTracker().set(PokemonEntity.Companion.getMOVING(), isMoving);
+        boolean isMoving = getEntity().getControllingPassenger() != null ?
+                getEntity().getControllingPassenger().getVelocity().lengthSquared() > minimumSpeed :
+                getEntity().getVelocity().lengthSquared() > minimumSpeed;
+
+        if (getEntity().getControllingPassenger() != null) {
+            System.out.println(isMoving + " " + getEntity().getVelocity().lengthSquared());
         }
+
+        getEntity().getDataTracker().set(PokemonEntity.Companion.getMOVING(), isMoving);
     }
 }
