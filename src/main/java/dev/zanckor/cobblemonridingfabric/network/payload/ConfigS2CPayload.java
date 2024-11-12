d56cb100cb642bf9abcc60f61ed8a32a0d007160
Zanckor <zanckor002@gmail.com>
2024-11-12 23:49:01 +0100

    Increased string max length on config packet length

--- src/main/java/dev/zanckor/cobblemonridingfabric/network/payload/ConfigS2CPayload.java
+++ src/main/java/dev/zanckor/cobblemonridingfabric/network/payload/ConfigS2CPayload.java
@@ -3,12 +3,13 @@ package dev.zanckor.cobblemonridingfabric.network.payload;
 import dev.zanckor.cobblemonridingfabric.network.NetworkHandler;
 import net.minecraft.network.RegistryByteBuf;
 import net.minecraft.network.codec.PacketCodec;
-import net.minecraft.network.codec.PacketCodecs;
 import net.minecraft.network.packet.CustomPayload;
+
+import static net.minecraft.network.codec.PacketCodecs.string;
 
 public record ConfigS2CPayload(String pokemonJson) implements CustomPayload {
     public static final CustomPayload.Id<ConfigS2CPayload> ID = new CustomPayload.Id<>(NetworkHandler.CONFIG_PACKET);
-    public static final PacketCodec<RegistryByteBuf, ConfigS2CPayload> CODEC = PacketCodec.tuple(PacketCodecs.STRING, ConfigS2CPayload::pokemonJson, ConfigS2CPayload::new);
+    public static final PacketCodec<RegistryByteBuf, ConfigS2CPayload> CODEC = PacketCodec.tuple(string(327670), ConfigS2CPayload::pokemonJson, ConfigS2CPayload::new);
 
     @Override
     public Id<? extends CustomPayload> getId() {
